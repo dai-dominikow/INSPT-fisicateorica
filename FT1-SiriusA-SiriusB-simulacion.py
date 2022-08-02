c802f6c291db24fe723c1203e4c24daddffd5394
Dei <daiana@airtm.io>
2022-08-02 14:40:40 -0300

    Add Sirius Binary Sistem Simulation

---
+++ FT1-SiriusA-SiriusB-simulacion.py
@@ -0,0 +1,61 @@
+from vpython import *
+
+#Fuerzas centrales!
+#Daiana Dominikow
+#Final Fisica Teorica 1
+
+G=6.67e-11      #constante universal
+rA=1.1903e6     #radio Sirius A
+rB= 5844e3      #radio Sirius B
+mA=3.978e30     #masa Sirius A
+mB=2.025e30     #masa Sirius B
+mu= mA*mB/(mA+mB)   #masa reducida
+d= 3e9       #distancia entre las estrellas
+
+#primero creo las esferas y les doy sus atributos
+SiriusA = sphere(pos = vector(0,0,0), 
+                radius=rA*100,    #multiplique 10 veces el radio para que se vea mejor
+                color=color.cyan, 
+                make_trail=True)
+                
+SiriusB = sphere(pos=vector(d,0,0),
+              radius=rB*100,
+              color=color.red,
+              make_trail=True
+              )
+
+#la velocidad de Sirius B
+vB=sqrt(G*mA/d)
+
+#momento inicial del sistema
+SiriusA.p = vector(0,0,0) 
+SiriusB.p = mB*vector(0,vB,0)  #p=m*v
+
+#defino mi tiempo
+t = 0
+dt = 360
+mes=24*3600*30
+
+#momento angular
+l=mag(cross(SiriusA.pos,SiriusA.p)+cross(SiriusB.pos,SiriusB.p))
+
+#seteo el gráfico de la energía en funcion a la posición
+grafico = graph(xtitle='Distancia (m)', ytitle = 'Energia (J)')
+fU = gcurve(color=color.green, dot=True) 
+
+#vamos a simular 6 meses con el metodo de Cromer - Euler
+while t<mes*6:
+    rate(50) #velocidad de la simulacion
+    r = SiriusB.pos - SiriusA.pos
+    F = -G*mA*mB*norm(r)/mag(r)**2 
+    SiriusB.p = SiriusB.p + F*dt
+    SiriusA.p = SiriusA.p - F*dt
+    SiriusB.pos = SiriusB.pos + SiriusB.p*dt/mB
+    SiriusA.pos = SiriusA.pos + SiriusA.p*dt/mA
+    
+    #para graficar la energía potencial efectiva del sistema
+    Ug = -G*mA*mB/mag(r)            #potencial gravitatoria
+    Uc = l**2 / (2*mu*mag(r)**2)    #potencial centrípeta
+    Uef = Ug+Uc                     #potencial efectiva
+    fU.plot(mag(r),Uef)
+    t = t + dt
