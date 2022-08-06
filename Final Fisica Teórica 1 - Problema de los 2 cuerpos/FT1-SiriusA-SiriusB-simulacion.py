3a36b122fd882336525b125d9cd1c722a95b97fd
Daiana Dominikow <daianadominikow@Daianas-MacBook-Air.local>
2022-08-06 10:28:16 -0300

    Agrego sistema Tierra Luna

--- Final Fisica Teórica 1 - Problema de los 2 cuerpos/FT1-SiriusA-SiriusB-simulacion.py
+++ Final Fisica Teórica 1 - Problema de los 2 cuerpos/FT1-SiriusA-SiriusB-simulacion.py
@@ -12,22 +12,26 @@ mB=2.025e30     #masa Sirius B
 mu= mA*mB/(mA+mB)   #masa reducida
 d= 3e9       #distancia entre las estrellas
 
+#posición centros de masa
+xA = -(mB/(mA+mB))*d
+xB = (mA/(mA+mB))*d
+
 #primero creo las esferas y les doy sus atributos
-SiriusA = sphere(pos = vector(0,0,0), 
+SiriusA = sphere(pos = vector(xA,0,0), 
                 radius=rA*100,    #multiplique 10 veces el radio para que se vea mejor
                 color=color.cyan, 
                 make_trail=True)
                 
-SiriusB = sphere(pos=vector(d,0,0),
+SiriusB = sphere(pos=vector(xB,0,0),
               radius=rB*100,
               color=color.red,
               make_trail=True
               )
 
 #Centro de masa!
-cm = (mA*SiriusA.pos+mB*SiriusB.pos)/(mA+mB)
-CM = sphere(pos=cm,
-            radius=0.1e9)
+# cm = (mA*SiriusA.pos+mB*SiriusB.pos)/(mA+mB)
+# CM = sphere(pos=cm,
+#             radius=0.1e9)
 
 #la velocidad de Sirius B
 vB=sqrt(G*mA**2/(d*(mA+mB)))
@@ -39,7 +43,7 @@ SiriusB.p = mB*vector(0,vB,0)  #p=m*v
 #defino mi tiempo
 t = 0
 dt = 360
-mes=24*3600*30
+t_f=24*3600*30
 
 #momento angular
 l=mag(cross(SiriusA.pos,SiriusA.p)+cross(SiriusB.pos,SiriusB.p))
@@ -48,10 +52,8 @@ l=mag(cross(SiriusA.pos,SiriusA.p)+cross(SiriusB.pos,SiriusB.p))
 grafico = graph(xtitle='Distancia (m)', ytitle = 'Energia (J)')
 fU = gcurve(color=color.green, dot=True) 
 
-
-
-#vamos a simular 6 meses con el metodo de Cromer - Euler
-while t<mes*6:
+# Metodo de Cromer - Euler
+while t<t_f:
     rate(20) #velocidad de la simulacion
     r = SiriusB.pos - SiriusA.pos
     F = -G*mA*mB*norm(r)/mag(r)**2 
@@ -59,8 +61,8 @@ while t<mes*6:
     SiriusA.p = SiriusA.p - F*dt
     SiriusB.pos = SiriusB.pos + SiriusB.p*dt/mB
     SiriusA.pos = SiriusA.pos + SiriusA.p*dt/mA
-    cm =  (mA*SiriusA.pos+mB*SiriusB.pos)/(mA+mB)#actualizo centro de masa
-    CM.pos = cm
+    # cm =  (mA*SiriusA.pos+mB*SiriusB.pos)/(mA+mB)#actualizo centro de masa
+    # CM.pos = cm
     #para graficar la energía potencial efectiva del sistema
     Ug = -G*mA*mB/mag(r)            #potencial gravitatoria
     Uc = l**2 / (2*mu*mag(r)**2)    #potencial centrípeta
